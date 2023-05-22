db568284090bb36fd83d20b34b7dcdd4c5c26a58
anubhavmishra800 <127845673+anubhavmishra800@users.noreply.github.com>
2023-05-22 14:00:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const size=document.createElement("h1");
+let div=document.getElementById("sizeInfo");
+size.innerText='Width:${window.innerWidth} and Height:${window.innerHeight}';
+div.append(size);
+
+window.onresize=function(){
+	size.innerText=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+};
